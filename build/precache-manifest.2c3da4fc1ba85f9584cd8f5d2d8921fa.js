8a711787f777a129a8ce07d170746bb92a271c4b
hiren-bacancytechnology <hiren.panchal@bacancytechnology.com>
2019-08-01 17:58:22 +0530

    demo

---
+++ build/precache-manifest.2c3da4fc1ba85f9584cd8f5d2d8921fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818ea2ee28b7e826b451f6cd1506dd92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "836d246069fa6e8a6347",
+    "url": "/static/css/main.8f6c3d69.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "836d246069fa6e8a6347",
+    "url": "/static/js/main.dc7201de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const convertFormData = async (data) => {
+// let formData = new FormData();
+// for (let k in data) {
+// await formData.append(k, data[k]);
+// }
+// return formData;
+// };
+class App extends Component {
+constructor(){
+super()
+this.state = {
+
+}
+}
+
+// handelChange = (e)=>{
+// e.preventDefault()
+// this.setState({
+// [e.target.name] : e.target.files[0]
+// })
+// }
+
+// handelSubmit = async (e)=>{
+// e.preventDefault()
+
+// let resp = await fetch("http://3.19.86.25:8080/api/earner/signup", {
+// method: 'POST',
+// body: await convertFormData(this.state)
+// })
+
+// // axios.post("http://3.19.86.25:8080/api/earner/signup",{
+// // headers: {
+// // 'Accept': '*/*',
+// // 'Content-Type': 'multipart/form-data',
+// // }
+// // }, await convertFormData(this.state))
+
+// } 
+componentDidMount(){
+// Load This Code At The End Of HTML File
+var iframeEl = document.getElementById('rz-iframe');
+setTimeout(function() {
+document.getElementById('rz-iframe').style.display = "inline"
+}, 1100);
+setTimeout(function(){
+iframeEl.contentWindow.postMessage("", 'http://3.19.86.25:8081/'); 
+}
+,1000)
+
+// function bindEvent(element, eventName, eventHandler) {
+//   if (element.addEventListener){
+//       element.addEventListener(eventName, eventHandler, false);
+//   } else if (element.attachEvent) {
+//       element.attachEvent('on' + eventName, eventHandler);
+//   }
+// }
+// // var iframeEl = document.getElementById('rz-iframe');
+// bindEvent(window, 'message', function (e) {
+// iframeEl.style.height = e.data.height;
+// iframeEl.style.width = e.data.width;
+// });
+
+}
+
+render(){
+return (
+<div className="App">
+<iframe src="http://3.19.86.25:8081/" id="rz-iframe" title="frame"  allowtransparency="true" ></iframe>
+{/* <form>
+<label>img1</label>
+<input type="file" name="frontSide" onChange = {(e)=> this.handelChange(e)} />
+<label>img1</label>
+<input type="file" name="backSide" onChange = {(e)=> this.handelChange(e)} />
+<label>img1</label>
+<input type="file" name="earnerImage" onChange = {(e)=> this.handelChange(e)} />
+<input type="submit" value="Upload Image" name="submit" onClick = {(e)=>this.handelSubmit(e)} />
+</form> */}
+</div>
+);
+}
+
 }
 
 export default App;
